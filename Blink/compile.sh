0ab9719afa3a7f92081471aa1fe43d210ebd07d8
Joshua Gopaul <evolvednucleus@gmail.com>
2022-10-05 20:48:31 -0400

    Delete Blink directory

--- Blink/compile.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-avr-gcc -Os -DF_CPU=16000000UL -mmcu=atmega328p -c -o bitwise.o bitwise.c
-avr-gcc -mmcu=atmega328p bitwise.o -o bitwise
-avr-objcopy -O ihex -R .eeprom bitwise bitwise.hex
-avrdude -F -V -c arduino -p ATMEGA328P -P /dev/ttyACM0 -b 115200 -U flash:w:bitwise.hex
